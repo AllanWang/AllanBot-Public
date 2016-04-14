25f364ff09a8ff307a3cd2fe391ffa839dbe2ea6
Allan W <asdfasdfvful@gmail.com>
2016-04-14 02:39:06 -0400

    t

--- src/translate.js
+++ src/translate.js
@@ -17,6 +17,9 @@ function translateRequest(fromLang, toLang, phrase, callback) {
 
 // Actual translation method
 function translate(fromLang, toLang, phrase) {
+    /*Sample url
+    https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=en&dt=t&q=[text]
+    */
     var options = {
         method: 'GET',
         url: 'https://translate.googleapis.com/translate_a/single',
@@ -25,14 +28,23 @@ function translate(fromLang, toLang, phrase) {
             sl: fromLang,
             tl: toLang,
             dt: 't',
-            q: phrase
+            q: encodeURIComponent(phrase)
+                // q: phrase
         },
         headers: {
             'cache-control': 'no-cache'
         }
     };
+    log.info('url', options)
     return rp(options).then(function(response) {
-        return response.match(/"(.*?)"/)[1]
+        var text = response.match(/"(.*?)"/)[1];
+        text = decodeURIComponent(text.replace(/% /g, '%'));
+        try {
+            log.info('response', JSON.stringify(response));
+        } catch (err) {
+            log.warn('response not decoded');
+        }
+        return text;
     });
 }
 
