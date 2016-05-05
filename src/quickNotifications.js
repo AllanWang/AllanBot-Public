7862833f8af0f9d2e558f17146b9edcf5e60f0d4
Allan W <allan.wang@mail.mcgill.ca>
2016-05-04 23:59:22 -0400

    untested rewrite of QN

--- src/quickNotifications.js
+++ src/quickNotifications.js
@@ -37,61 +37,60 @@ function notifyData(api, message) {
 }
 
 function addnotifyData(api, message, input) {
+    if (input.slice(0, 1) != '@') return;
     var fullText = input.slice(1);
     var i = fullText.indexOf(":");
-    if (i < 1) {
-        return;
-    }
-    var name = fullText.slice(0, i);
-    fullText = fullText.slice(i + 1);
-    fullText = fullText.trim();
+    if (i < 1) return;
+    var nameOrig = fullText.slice(0, i);
+    fullText = fullText.slice(i + 1).trim();
+    var name = nameOrig.toLowerCase();
+    if (name == v.botNameL) return api.sendMessage('You cannot send a notification to ' + botNameL + '.', message.threadID);
 
-    var nameOrig = name;
-    name = name.toLowerCase();
     api.getUserInfo(message.senderID, function(err, ret1) {
         if (err) return console.error(err);
         var intro = ret1[message.senderID].firstName + " to " + nameOrig + ": ";
         var text = 'placeholder_text';
         var finalID = 'placeholder_id';
+        var count = 0;
+        var self = false;
 
         api.getThreadInfo(message.threadID, function callback(err, info) {
             if (err) return console.error(err);
-            if (name == v.botNameL) {
-                api.sendMessage('You cannot send a notification to ' + botNameL + '.', message.threadID);
-            } else {
-                api.getUserInfo(info.participantIDs, function(err, ret) {
-                    if (err) return console.error(err);
-                    for (var id in ret) {
-                        if (ret.hasOwnProperty(id)) {
-                            var participantName = ret[id].name.toLowerCase();
-                            if (v.contains(participantName, name) && ret[id].firstName.toLowerCase() != v.botNameL) {
-                                // Match. Do matched stuff.
-                                if (id == message.senderID) {
-                                    var text = 'You cannot send notifications to yourself.';
-                                    api.sendMessage(text, message.threadID);
-                                    log.info(text);
-                                } else if (id == v.botID) {
-                                    log.info('ignore id for own bot');
-                                } else {
-                                    log.info("correct id is " + id);
-                                    try {
-                                        if (v.sBase.notificationMessages[message.threadID][id]) {
-                                            text = v.sBase.notificationMessages[message.threadID][id] + '\n' + fullText;
-                                            f.setData(api, message, v.f.Notifications.child(message.threadID).child(id), text, 'Notification saved:\n' + fullText);
-                                        } else {
-                                            text = intro + fullText;
-                                            f.setData(api, message, v.f.Notifications.child(message.threadID).child(id), text, 'Notification saved:\n' + fullText);
-                                        }
-                                    } catch (err) {
+            api.getUserInfo(info.participantIDs, function(err, ret) {
+                if (err) return console.error(err);
+                for (var id in ret) {
+                    if (ret.hasOwnProperty(id)) {
+                        var participantName = ret[id].name.toLowerCase();
+                        if (v.contains(participantName, name) && ret[id].firstName.toLowerCase() != v.botNameL) {
+                            // Match. Do matched stuff.
+                            count++;
+                            if (id == message.senderID) {
+                                self = true; //for future notification
+                            } else if (id == v.botID) {
+                                log.info('ignore id for own bot');
+                            } else {
+                                log.info("correct id is " + id);
+                                try {
+                                    if (v.sBase.notificationMessages[message.threadID][id]) {
+                                        text = v.sBase.notificationMessages[message.threadID][id] + '\n' + fullText;
+                                    } else {
                                         text = intro + fullText;
-                                        f.setData(api, message, v.f.Notifications.child(message.threadID).child(id), text, 'Notification saved:\n' + fullText);
                                     }
+                                } catch (err) {
+                                    text = intro + fullText;
+                                } finally {
+                                    f.setData(api, message, v.f.Notifications.child(message.threadID).child(id), text, 'Notification saved for ' + ret[id].name + ':\n' + fullText);
                                 }
                             }
                         }
                     }
-                });
-            }
+                }
+                if (self && count == 1) {
+                    var ss = 'You cannot send notifications to yourself.';
+                    api.sendMessage(ss, message.threadID);
+                    log.info(ss);
+                }
+            });
         });
     });
 }
